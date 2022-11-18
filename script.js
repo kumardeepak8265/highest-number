3a67b957be9cc5846852a64d0d987bee2fbde49f
deepak kumar <kumardeepak6830@gmail.com>
2022-11-18 21:21:44 +0530

    alert add

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-console.log("runing");
 let highest;
-for (let i = 1; i > -99999; i++) {
-  let number = prompt("enter the number");
+
+for (let i = 1; i > 0; i++) {
+  let number = prompt(`next number`);
+  console.log("number", number, "typeof number", typeof number);
   if (number === null) {
+    if (i === 1) {
+      alert("Please First time not  press Enter Cancle button ");
+    }
     break;
   }
   if (number === ``) {
+    alert("Please no Enter empty OK Button ");
     continue;
   }
-  if (Object.is(number, NaN)) {
-    continue;
-  }
+
   number = +number;
   if (highest === undefined || number > highest) {
     highest = number;
   }
-  console.log("highest", highest);
   document.getElementById("data").innerHTML = highest;
 }
